e8be024b56fee2ef398086c1c52b80bed01b9bf5
DongKyu Ryu <fbehdrb97@naver.com>
2023-12-06 14:57:41 +0900

    2023-12-06 14:57 백업 및 최신화

--- src/main/java/NewRegist/NewRegistDAO.java
+++ src/main/java/NewRegist/NewRegistDAO.java
@@ -5,12 +5,12 @@ import java.util.Map;
 
 import DBcontrol.DBConnPool;
 
-public class NewRegistDAO extends DBConnPool{
-	public NewRegistDTO NewRegist (String name, String emp_num, String emp_grade, String team) {
+public class NewRegistDAO extends DBConnPool {
+	public NewRegistDTO NewRegist(String name, String emp_num, String emp_grade, String team) {
 		String query = "INSERT INTO emp (name, emp_num, emp_grade, team) VALUES (?, ?, ?, ?)";
-		
+
 		System.out.println("정보등록 쿼리 실행");
-		
+
 		try {
 			// 쿼리 실행
 			psmt = con.prepareStatement(query);
@@ -18,12 +18,12 @@ public class NewRegistDAO extends DBConnPool{
 			psmt.setString(2, emp_num);
 			psmt.setString(3, emp_grade);
 			psmt.setString(4, team);
-			
+
 			System.out.println("[" + name + "]");
 			System.out.println("[" + emp_num + "]");
 			System.out.println("[" + emp_grade + "]");
 			System.out.println("[" + team + "]");
-			
+
 			psmt.executeUpdate();
 			System.out.println("정보 등록 완료");
 		} catch (Exception e) {
@@ -32,5 +32,24 @@ public class NewRegistDAO extends DBConnPool{
 		}
 		return null;
 	}
-	
+
+	// 게시물 수를 가져오는 쿼리
+	public int selectCount(Map<String, Object> map) {
+		int totalCount = 0;
+
+		String query = "SELECT COUNT(*) FROM holiday_check";
+		try {
+			// 쿼리 실행
+			stmt = con.createStatement();
+
+			rs = stmt.executeQuery(query);
+			if (rs.next()) {
+				totalCount = rs.getInt(1);
+			}
+		} catch (Exception e) {
+			System.out.println("게시물 수를 구하는 중 예외 발생");
+			e.printStackTrace();
+		}
+		return totalCount;
+	}
 }
