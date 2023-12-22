d94fa5b3d3ea0c4cc3fc76eb1299780f001c4fd4
21-manic-010g <21-manic-010g@21-manic-010>
2023-12-22 10:55:35 +0900

    날짜가 안 들어와짐

--- src/main/java/Controller/CalenderPlusController.java
+++ src/main/java/Controller/CalenderPlusController.java
@@ -2,12 +2,12 @@ package Controller;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import Calender.CalenderDAO;
 
@@ -22,16 +22,43 @@ public class CalenderPlusController extends HttpServlet {
 		System.out.println("");
 		
 		CalenderDAO dao = new CalenderDAO();
+		HttpSession session = request.getSession();
+		
 		
 	    // 클라이언트로부터 전송된 일정 파라미터를 받아옵니다.
 	    String newSchedule = request.getParameter("userInput");
 	        
 	    System.out.println("추가한 일정 값 확인: " + newSchedule);
 	    
+	    String selectedDay = (String) session.getAttribute("selectedDay");
+	    if (selectedDay != null && !selectedDay.isEmpty()) {
+		    // selectedDay가 null이 아니고 비어있지 않은 경우에만 변환 시도
+		    int dayValue;
+		    try {
+		        dayValue = Integer.parseInt(selectedDay);
+
+		        // 변환된 값이 10보다 작으면 앞에 0을 붙여서 문자열로 만듦
+		        if (dayValue < 10) {
+		            selectedDay = "0" + dayValue;
+		        }
+		    } catch (NumberFormatException e) {
+		    	
+		    }
+		} else {
+		    // selectedDay가 null이거나 비어있는 경우 처리
+		    // 적절한 로깅 또는 예외 처리를 수행
+		}
+			
+	    
+        String selectedYear = (String) session.getAttribute("selectedYear");
+        String selectedMonth = (String) session.getAttribute("selectedMonth");
+        String selecteddate = selectedYear + "-" + selectedMonth + "-" + selectedDay;
+	    
 	    // selecteddate와 emp_num을 HttpServletRequest에서 가져와서 사용
-	    String selecteddate = (String) request.getAttribute("selecteddate");
+	    //String selecteddate = (String) request.getSession().getAttribute("selecteddate");
 	    String emp_num = (String) request.getSession().getAttribute("loginid");
-	   
+	 
+	    
 	    System.out.println("가져온 값 확인: " + selecteddate + " " + emp_num);
 	    
 		 if (newSchedule != null) {
@@ -46,7 +73,8 @@ public class CalenderPlusController extends HttpServlet {
 	        }
 
 	   
-	        
+		 request.setAttribute("selecteddate", selecteddate);   
+		 
 	   
 	    
 	    }
